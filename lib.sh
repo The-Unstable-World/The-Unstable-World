4757610c0592043685de19ae71d7d22e21a0f22c
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2019-11-30 18:09:21 +0800

    更

--- lib.sh
+++ lib.sh
@@ -50,19 +50,18 @@ install_minetest_debian_builddeps_nosudo(){
 }
 
 install_minetest_windows_vcpkg_and_builddeps(){
-  if [ ! -d ~/vcpkg ];then
-    git clone https://github.com/Microsoft/vcpkg.git ~/vcpkg
-    cd ~/vcpkg
-    ./bootstrap-vcpkg.bat
-    cd -
-    ~/vcpkg/vcpkg install irrlicht zlib curl[winssl] openal-soft libvorbis libogg sqlite3 freetype luajit --triplet x64-windows
-  fi
+  git clone https://github.com/Microsoft/vcpkg.git ~/vcpkg
+  cd ~/vcpkg
+  ./bootstrap-vcpkg.bat
+  cd -
+  ~/vcpkg/vcpkg install irrlicht zlib curl[winssl] openal-soft libvorbis libogg sqlite3 freetype luajit --triplet x64-windows
 }
 
 build_minetest_client_windows(){
   cd ./minetest
   cmake . -G"Visual Studio 15 2017 Win64" -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_GETTEXT=0 -DENABLE_CURSES=0
   cmake --build . --config Release -- -j2
+  cpack -C Release --config ./CPackConfig.cmake
   7z a ./minetest.zip .
   cd ..
 }
