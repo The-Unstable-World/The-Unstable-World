dcd53f4025971658e6946866b50987f99bf160f4
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2019-12-14 00:25:07 +0800

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -3,9 +3,9 @@ COMMENT(){
 }
 
 get_minetest(){
-  git clone --depth=1 https://github.com/minetest/minetest.git ./minetest
-  git clone --depth=1 https://github.com/minetest/minetest_game.git ./minetest/games/minetest_game
-  rm -fr ./minetest/games/minetest_game/.git
+  (git clone --depth=1 https://github.com/minetest/minetest.git ./minetest &&
+  git clone --depth=1 https://github.com/minetest/minetest_game.git ./minetest/games/minetest_game &&
+  rm -fr ./minetest/games/minetest_game/.git)
 }
 
 mods_mod(){
@@ -51,13 +51,13 @@ get_mods__and__gen_WORLD-MT-CONFIG(){
   for x in $(mods_mod);do
     local x_base="${x##*/}"
     local x_name="${x_base%.git}"
-    git clone "$x"
+    git clone "$x" || return 1
     rm -fr "$x_name"/.git
   done
 
   for x in $(mods_modpack);do
     rm -fr MODPACK-TMP
-    git clone "$x" MODPACK-TMP
+    git clone "$x" MODPACK-TMP || return 1
     for subdir in MODPACK-TMP/*;do
       if [ -f "$subdir/mod.conf" ] || [ -f "$subdir/init.lua" ];then
         mv "$subdir" ./
@@ -108,22 +108,22 @@ install_minetest_mingw_builddeps__and__build__ubuntu1604(){
     WIN_ARCH=i686
     WIN_BITS=32
   fi
-  get_minetest
-  sudo apt-get install -y p7zip-full wget unzip git cmake gettext
-  wget "http://minetest.kitsunemimi.pw/mingw-w64-${WIN_ARCH}_7.1.1_ubuntu14.04.7z" -O mingw.7z
-  sed -e "s|%PREFIX%|${WIN_ARCH}-w64-mingw32|" -e "s|%ROOTPATH%|/usr/${WIN_ARCH}-w64-mingw32|" < ./minetest/util/travis/toolchain_mingw.cmake.in > ./minetest/util/buildbot/toolchain_mingw.cmake
-  sudo 7z x -y -o/usr mingw.7z
-  EXISTING_MINETEST_DIR="$PWD/minetest/" NO_MINETEST_GAME=1 "./minetest/util/buildbot/buildwin${WIN_BITS}.sh" build
-  unzip ./minetest/_build/minetest-*-win*.zip -d .
-  mv minetest-*-win* minetest-win
+  (get_minetest &&
+  sudo apt-get install -y p7zip-full wget unzip git cmake gettext &&
+  wget "http://minetest.kitsunemimi.pw/mingw-w64-${WIN_ARCH}_7.1.1_ubuntu14.04.7z" -O mingw.7z &&
+  sed -e "s|%PREFIX%|${WIN_ARCH}-w64-mingw32|" -e "s|%ROOTPATH%|/usr/${WIN_ARCH}-w64-mingw32|" < ./minetest/util/travis/toolchain_mingw.cmake.in > ./minetest/util/buildbot/toolchain_mingw.cmake &&
+  sudo 7z x -y -o/usr mingw.7z &&
+  EXISTING_MINETEST_DIR="$PWD/minetest/" NO_MINETEST_GAME=1 "./minetest/util/buildbot/buildwin${WIN_BITS}.sh" build &&
+  unzip ./minetest/_build/minetest-*-win*.zip -d . &&
+  mv minetest-*-win* minetest-win &&
   cp /usr/"${WIN_ARCH}"-w64-mingw32/bin/libgcc*.dll \
     /usr/"${WIN_ARCH}"-w64-mingw32/bin/libstdc++*.dll \
-    /usr/"${WIN_ARCH}"-w64-mingw32/bin/libwinpthread*.dll minetest-win/bin
-  7z a ./minetest/minetest.zip minetest-win/*
+    /usr/"${WIN_ARCH}"-w64-mingw32/bin/libwinpthread*.dll minetest-win/bin &&
+  7z a ./minetest/minetest.zip minetest-win/*)
 }
 
 build_minetest_client_osx(){
-  cd ./minetest
+  (cd ./minetest &&
   cmake . \
     -DVERSION_EXTRA="dev-$(sw_vers -productVersion)" \
     -DCMAKE_BUILD_TYPE=Release \
@@ -134,55 +134,55 @@ build_minetest_client_osx(){
     -DBUILD_CLIENT=TRUE \
     -DBUILD_SERVER=FALSE \
     -DCUSTOM_GETTEXT_PATH=/usr/local/opt/gettext \
-    -DCMAKE_EXE_LINKER_FLAGS="-L/usr/local/lib"
-  make -j4 package
-  cd ..
+    -DCMAKE_EXE_LINKER_FLAGS="-L/usr/local/lib" &&
+  make -j4 package)
 }
 
 build_minetest_client_gnulinux_amd64(){
-  cd ./minetest
+  (cd ./minetest &&
   cmake . \
     -DCMAKE_INSTALL_PREFIX=/usr \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_CLIENT=TRUE \
-    -DBUILD_SERVER=FALSE
-  make -j4
-  make install DESTDIR=minetest.AppDir
-  wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-  chmod +x linuxdeploy-x86_64.AppImage
-  ./linuxdeploy-x86_64.AppImage --appdir minetest.AppDir
-  wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
-  chmod +x appimagetool-x86_64.AppImage
-  ./appimagetool-x86_64.AppImage minetest.AppDir minetest.AppImage
-  cd ..
+    -DBUILD_SERVER=FALSE &&
+  make -j4 &&
+  make install DESTDIR=minetest.AppDir &&
+  wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage &&
+  chmod +x linuxdeploy-x86_64.AppImage &&
+  ./linuxdeploy-x86_64.AppImage --appdir minetest.AppDir &&
+  wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage &&
+  chmod +x appimagetool-x86_64.AppImage &&
+  ./appimagetool-x86_64.AppImage minetest.AppDir minetest.AppImage)
 }
 
 build_minetest_server_gnulinux_amd64(){
-  cd ./minetest
+  (cd ./minetest &&
   cmake . \
     -DCMAKE_INSTALL_PREFIX=/usr \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SERVER=TRUE \
-    -DBUILD_CLIENT=FALSE
-  make -j4
-  make install DESTDIR=minetestserver.AppDir
-  wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-  chmod +x linuxdeploy-x86_64.AppImage
-  sed -i 's/^Exec=minetest$/Exec=minetestserver/' ./minetestserver.AppDir/usr/share/applications/net.minetest.minetest.desktop
-  ./linuxdeploy-x86_64.AppImage --appdir minetestserver.AppDir
-  wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
-  chmod +x appimagetool-x86_64.AppImage
-  ./appimagetool-x86_64.AppImage minetestserver.AppDir minetestserver.AppImage
-  cd ..
+    -DBUILD_CLIENT=FALSE &&
+  make -j4 &&
+  make install DESTDIR=minetestserver.AppDir &&
+  rm ./minetestserver.AppDir/usr/share/applications/* &&
+  echo '[Desktop Entry]
+Name=MinetestServer
+Exec=minetestserver' > ./minetestserver.AppDir/usr/share/applications/minetestserver.desktop &&
+  wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage &&
+  chmod +x linuxdeploy-x86_64.AppImage &&
+  ./linuxdeploy-x86_64.AppImage --appdir minetestserver.AppDir &&
+  wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage &&
+  chmod +x appimagetool-x86_64.AppImage &&
+  ./appimagetool-x86_64.AppImage minetestserver.AppDir minetestserver.AppImage)
 }
 
 job_mods(){
-  mkdir mods
-  cd mods
-  get_mods__and__gen_WORLD-MT-CONFIG
-  mv WORLD-MT-CONFIG ..
-  zip -r ../mods.zip .
-  tar -czvf ../mods.tgz .
-  cd ..
-  rm -fr mods
+  (mkdir mods &&
+  cd mods &&
+  get_mods__and__gen_WORLD-MT-CONFIG &&
+  mv WORLD-MT-CONFIG .. &&
+  zip -r ../mods.zip . &&
+  tar -czvf ../mods.tgz . &&
+  cd .. &&
+  rm -fr mods)
 }
